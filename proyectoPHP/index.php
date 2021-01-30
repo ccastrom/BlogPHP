bbba2d0dae6a3886889ec2b9b3640761a39bec5f
ccastrom <claudio_jr@live.cl>
2021-01-30 13:32:03 -0300

    HTML structure complete on "index.php"

--- proyectoPHP/index.php
+++ proyectoPHP/index.php
@@ -6,22 +6,111 @@
         <title>Blog de videojuegos</title>
     </head>
     <body>
-       <!-- Cabezera -->
-       <header>
-           <div id="logo">
-               <a href="index.php">
-                   <h1>Blog de videojuegos</h1>
-               </a>
-           </div>
-       </header>
-       
-       <!-- Menu -->
-       
-       <!-- Barra lateral -->
-       
-       
-       <!-- Caja principal -->
-       
-       <!-- Pie de pagina -->
+        <!-- Cabecera -->
+        <header id="cabecera">
+            <!-- LOGO -->
+            <div id="logo">
+                <a href="index.php">
+                    Blog de videojuegos
+                </a>
+            </div>
+            <nav id="menu">
+                <ul>
+                    <li>
+                        <a href="index.php">Inicio</a>
+                    </li>
+                    <li>
+                        <a href="index.php">Categoria 1</a>
+                    </li>
+                    <li>
+                        <a href="index.php">Categoria 2</a>
+                    </li>
+                    <li>
+                        <a href="index.php">Categoria 3</a>
+                    </li>
+                    <li>
+                        <a href="index.php">Categoria 4</a>
+                    </li>
+                    <li>
+                        <a href="index.php">Sobre mi</a>
+                    </li>
+                    <li>
+                        <a href="index.php">Contacto</a>
+                    </li>
+                </ul>
+
+            </nav>
+        </header>
+        <div id="contenedor">
+            <!-- Barra lateral -->
+            <aside id="sidebar">
+                <div id="login" class="bloque">
+                    <h3>Identificate</h3>
+                    <form action="login.php" method="POST">
+                        <label for="email">email</label>
+                        <input type="email" name="email"/>
+
+                        <label for="password">contraseña</label>
+                        <input type="password" name="password"/>
+                        <input type="submit" value="Entrar">
+                    </form>
+                </div>
+
+                <div id="register" class="bloque">
+                    <h3>Registrate</h3>
+                    <form action="registro.php" method="POST">
+                        <label for="nombre">Nombre</label>
+                        <input type="text" name="nombre "/>
+
+                        <label for="apellidos">Apellidos</label>
+                        <input type="text" name="apellidos"/>
+
+                        <label for="email">email</label>
+                        <input type="email" name="email"/>
+
+                        <label for="password">contraseña</label>
+                        <input type="password" name="password"/>
+                        <input type="submit" value="Registrarse">
+                    </form>
+                </div>
+            </aside>
+            <!-- Caja principal -->
+            <div id="principal">
+                <h1>Ultimas entradas</h1>
+                <article class="entrada">
+                    <h2>Titulo de mi entrada</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing 
+                        elit. Maecenas consectetur.                      
+                    </p>
+                </article>
+                <article class="entrada">
+                    <h2>Titulo de mi entrada</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing 
+                        elit. Maecenas consectetur.                      
+                    </p>
+                </article>
+                <article class="entrada">
+                    <h2>Titulo de mi entrada</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing 
+                        elit. Maecenas consectetur.                      
+                    </p>
+                </article>
+                <article class="entrada">
+                    <h2>Titulo de mi entrada</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing 
+                        elit. Maecenas consectetur.                      
+                    </p>
+                </article>
+
+            </div>
+        </div>
+        <!-- Pie de pagina -->
+        <footer id="pie">
+            <p>Desarrollado por Claudio Castro &copy; 2021</p>
+        </footer>
     </body>
 </html>
