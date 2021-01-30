f97359662015f182a7ca8b3049fd01202ec7d907
ccastrom <claudio_jr@live.cl>
2021-01-30 12:58:27 -0300

    Basic files

---
+++ proyectoPHP/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Blog de videojuegos</title>
+    </head>
+    <body>
+       <!-- Cabezera -->
+       
+       <!-- Menu -->
+       
+       <!-- Barra lateral -->
+    </body>
+</html>
