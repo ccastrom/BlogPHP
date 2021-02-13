c09f3a1385c1c444de07a30ecdf1e25729225a6a
ccastrom <claudio_jr@live.cl>
2021-02-12 21:44:04 -0300

    LAST FOUR ENTRYS OF THE BLOG COMPLETED -Generated function 'conseguirUltimasEntradas($conexion)' 	to get the sql sentence in 'helpers.php' -Validated the variable '$categorias' in case if is not empty in 	'cabecera.php' -The sql query was fetched with variable '$entradas' and listed 	with variable '$entrada' in 'index.php'

--- proyectoPHP/includes/helpers.php
+++ proyectoPHP/includes/helpers.php
@@ -1,31 +1,45 @@
 <?php
-function mostrarError($errores,$campo){
-    $alerta='';
-    if(isset($errores[$campo])&&!empty($campo)){
-        $alerta="<div class='alerta alerta-error'>".$errores[$campo].'</div>';
-        
-    }
-    return $alerta;
+
+function mostrarError($errores, $campo){
+$alerta = '';
+if(isset($errores[$campo])&&!empty($campo)){
+$alerta = "<div class='alerta alerta-error'>".$errores[$campo].'</div>';
+
+}
+return $alerta;
 }
 function borrarErrores(){
-    $borrado=false;
-    if(isset($_SESSION['errores'])){
-        unset($_SESSION['errores']);
-    }
-    
-    if(isset($_SESSION['completado'])){
-        unset($_SESSION['completado']);
-    }
-    return $borrado;
-    
-    
+$borrado = false;
+if(isset($_SESSION['errores'])){
+unset($_SESSION['errores']);
+}
+
+if(isset($_SESSION['completado'])){
+unset($_SESSION['completado']);
+}
+return $borrado;
+
+
 }
 function conseguirCategorias($conexion){
-    $sql= "SELECT * FROM categorias ORDER BY id ASC;";
-    $categorias=mysqli_query($conexion, $sql);
-    $result=array();
-    if($categorias && mysqli_num_rows($categorias)>=1){
-        $result=$categorias;
+$sql = "SELECT * FROM categorias ORDER BY id ASC;";
+$categorias = mysqli_query($conexion, $sql);
+$result = array();
+if($categorias && mysqli_num_rows($categorias) >= 1){
+$result = $categorias;
+}
+return $result;
+}
+function conseguirUltimasEntradas($conexion){
+$sql = "SELECT e.*,c.* FROM entradas e
+INNER JOIN categorias c ON e.categoria_fk_id=c.id
+ORDER BY e.id DESC LIMIT 4;";
+    $entradas= mysqli_query($conexion, $sql);
+    
+    $resultado= array();
+
+    if($entradas && mysqli_num_rows($entradas)>=1){
+        $resultado=$entradas;
     }
-    return $result;
+    return $entradas;
 }
--- proyectoPHP/index.php
+++ proyectoPHP/index.php
@@ -1,53 +1,40 @@
-<?php require_once 'includes/cabecera.php';?>
-<?php require_once './includes/lateral.php';?>       
-          
-            <!-- Caja principal -->
-            <div id="principal">
-                <h1>Ultimas entradas</h1>
-                <article class="entrada">
-                    <a href="">
-                        <h2>Titulo de mi entrada</h2>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing 
-                            elit. Maecenas consectetur.                      
-                        </p>
-                    </a>
+<?php require_once 'includes/cabecera.php'; ?>
+<?php require_once './includes/lateral.php'; ?>       
 
-                </article>
-                <article class="entrada">
-                    <a href="">
-                        <h2>Titulo de mi entrada</h2>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing 
-                            elit. Maecenas consectetur.                      
-                        </p>
-                    </a>
-                </article>
-                <article class="entrada">
-                    <a href="">
-                        <h2>Titulo de mi entrada</h2>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing 
-                            elit. Maecenas consectetur.                      
-                        </p>
-                    </a>
-                </article>
-                <article class="entrada">
-                    <a href="">
-                        <h2>Titulo de mi entrada</h2>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing 
-                            elit. Maecenas consectetur.                      
-                        </p>
-                    </a>
-                </article>
-                <div id="ver-todas">
-                    <a href="">Ver todas las entradas</a>
-                </div>
+<!-- Caja principal -->
+<div id="principal">
+    <h1>Ultimas entradas</h1>
+    <?php
+    $entradas = conseguirUltimasEntradas($db);
+    if (!empty($entradas)):
+        
+        while ($entrada = mysqli_fetch_assoc($entradas)):
+            
+            ?>
+            <article class="entrada">
+                <a href="">
+                    
+                    <h2><?=$entrada['titulo']?></h2>
+                    <p>
+                        <?= substr($entrada['descripcion'], 0,180)."..."?>
+                    </p>
+                </a>
+            </article>
 
-            </div>
-            <!-- fin principal-->
-            
-            
+            <?php
+        endwhile;
+
+    endif;
+    ?>
+
+    
+    <div id="ver-todas">
+        <a href="">Ver todas las entradas</a>
+    </div>
+
+</div>
+<!-- fin principal-->
+
+
 <?php require_once './includes/pie.php'; ?>
 
